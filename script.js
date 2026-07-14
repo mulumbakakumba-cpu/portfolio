4d5d3ca466c9b654004eee8bbe3096727fb7c3b9
mulumbakakumba-cpu <mulumba.kakumba@final.edu.tr>
2026-07-14 23:02:29 +0100

    Initial portfolio

---
+++ script.js
@@ -0,0 +1,38 @@
+const words = [
+    "Web Developer",
+    "Data Analyst",
+    "IT Support Specialist",
+    "Problem Solver"
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typing = document.getElementById("typing");
+
+function typeEffect() {
+
+    const currentWord = words[wordIndex];
+
+    if (!isDeleting) {
+        typing.textContent = currentWord.substring(0, charIndex++);
+    } else {
+        typing.textContent = currentWord.substring(0, charIndex--);
+    }
+
+    if (!isDeleting && charIndex === currentWord.length + 1) {
+        isDeleting = true;
+        setTimeout(typeEffect, 1200);
+        return;
+    }
+
+    if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        wordIndex = (wordIndex + 1) % words.length;
+    }
+
+    setTimeout(typeEffect, isDeleting ? 50 : 100);
+}
+
+typeEffect();
